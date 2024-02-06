7fcf49d727ce0b23b16844bd8d7dbe4dbbff66f1
Aliu Adedigba <indexcorpng@gmail.com>
2024-02-06 14:27:53 +0100

    uodat

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'web2',
+  appId: 'sbox.chronicles.v2',
+  appName: 'SuccessBOX 2.0',
   webDir: 'www',
   server: {
     androidScheme: 'https'
